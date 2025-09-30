53f49ae47214046ba5203103c18311d052427766
Tom Moran <62551154+Tomhausen@users.noreply.github.com>
2025-09-30 11:23:41 +0100

    Initial files for MakeCode project

---
+++ ArrayHelpers.ts
@@ -0,0 +1,11 @@
+namespace Array {
+
+    //% blockId=copy
+    //% block="scroll tilemap at speed $scrollSpeed when mouse $buffer from edge"
+    //% group="Operations"
+    //% weight=0
+    export function copy(array: any[]): any[] {
+        return array.slice();
+    }
+
+}
